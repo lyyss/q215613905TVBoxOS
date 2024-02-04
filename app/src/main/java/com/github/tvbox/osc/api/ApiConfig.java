d9323a7bbdf8108f155898eeb9914879f34d34ba
lyyss <lyyss@163.com>
2024-02-04 15:30:28 +0800

    Update ApiConfig.java
    https://gitee.com/max-min/m3u8/raw/master/n1.json

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitee.com/max-min/m3u8/raw/master/n1.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
